8d32e7098f8affe7c1f61b07c8d4ab6c5dbc708d
BeDCAF <balakin.arseny2017@gmail.com>
2025-05-23 15:34:33 +0500

    Добавил структуру и несколько библеотек для работы с ETL

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
-#include <fstream>
+#include <windows.h> // Работа с Windows API 
+#include <evntrace.h> // Функции для работы с Event Tracing for Windows
+#include <fstream>// Работа с файлами
 #include <vector>
-#include <string>
-#include <sstream>
-#include <filesystem>
-#include <windows.h>
-#include <cstdlib>
-#include "json.hpp" // Для будующей конвертации в json
+#include <iostream>
+#include <sstream> // Работа со строковыми потоками
+#include <iomanip> // Форматирование вывода
+#include <tdh.h> // Функции для работы с данными событий (Trace Data Helper)
 
-using json = nlohmann::json;
 using namespace std;
-namespace fs = std::filesystem;
 
 // Получение пути до ETL файла
 bool getETLFilePath(string& etlPath) {
@@ -18,6 +15,22 @@ bool getETLFilePath(string& etlPath) {
     getline(cin, etlPath);
     return fs::exists(etlPath) && !fs::is_directory(etlPath);
 }
+
+// Структура для хранения данных о событии
+struct EventData {
+    ULONG EventID; // Идентификатор события
+    GUID ProviderID; // GUID провайдера события
+    ULONG ProcessID; // Идентификатор процесса
+    ULONG ThreadID; // Идентификатор потока
+    ULONGLONG Timestamp; // Временная метка
+    ULONG KernelTime; // Время выполнения в ядре
+    ULONG UserTime; // Время выполнения в пользовательском режиме
+    UCHAR EventVersion; // Версия события
+    UCHAR EventOpcode; // Код операции события
+    UCHAR EventLevel;  // Уровень важности события
+    ULONGLONG EventKeyword; // Ключевые слова события
+    std::wstring TimeLocal; // Локализованное время в строковом формате
+};
 
 int main() {
     
