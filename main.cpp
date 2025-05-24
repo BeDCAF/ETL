50a41602ff3e9fe23cadf49f7dd2e3b71b3a91d7
BeDCAF <balakin.arseny2017@gmail.com>
2025-05-24 18:06:34 +0500

    Начало реализации класса ETLParser, другой метод получения пути

--- main.cpp
+++ main.cpp
@@ -7,14 +7,10 @@
 #include <iomanip> // Форматирование вывода
 #include <tdh.h> // Функции для работы с данными событий (Trace Data Helper)
 
-using namespace std;
 
-// Получение пути до ETL файла
-bool getETLFilePath(string& etlPath) {
-    cout << "Введите путь до ETL файла: ";
-    getline(cin, etlPath);
-    return fs::exists(etlPath) && !fs::is_directory(etlPath);
-}
+#pragma comment(lib, "advapi32.lib") //Для работы с ETW
+#pragma comment(lib, "tdh.lib") // Trace Data Helper
+#pragma comment(lib, "ole32.lib") // Для работы с GUID
 
 // Структура для хранения данных о событии
 struct EventData {
@@ -32,18 +28,40 @@ struct EventData {
     std::wstring TimeLocal; // Локализованное время в строковом формате
 };
 
+// Класс для парсинга ETL-файлов
+class ETLParser {
+public:
+    ETLParser() {}
+
+    bool Parse(const std::wstring& filename) {
+        filename_ = filename;
+        EVENT_TRACE_LOGFILE logFile = { 0 };
+        logFile.LogFileName = const_cast<LPWSTR>(filename_.c_str());
+        logFile.ProcessTraceMode = PROCESS_TRACE_MODE_EVENT_RECORD | PROCESS_TRACE_MODE_RAW_TIMESTAMP;
+        logFile.Context = this;
+
+        TRACEHANDLE traceHandle = OpenTrace(&logFile);
+        if (traceHandle == INVALID_PROCESSTRACE_HANDLE) {
+            std::cerr << "OpenTrace failed: " << GetLastError() << std::endl;
+            return false;
+        }
+
+        ULONG status = ProcessTrace(&traceHandle, 1, NULL, NULL);
+        CloseTrace(traceHandle);
+        return status == ERROR_SUCCESS;
+    }
+
+private:
+    std::wstring filename_;
+    std::vector<EventData> events_;
+};
+
 int main() {
     
     // UTF-8 в консоли
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
-
-    string etlPath;
-    if (!getETLFilePath(etlPath)) {
-        cerr << "Ошибка: ETL-файл не найден или это директория." << endl;
-        return 1;
-    }
-
+    
     return 0;
 }
 
